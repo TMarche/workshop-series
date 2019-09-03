8a60b2486ed21c080911764bb84dd0bb30add6f5
Matt Corbett <matthew.corbett@willowtreeapps.com>
2019-09-03 11:44:50 -0400

    Update Views for Module 2 End
    - Update dependencies so we can use Material library
    - Change App Theme for Material library
    - Add TextWatcher to MainActivity
    - Update views in `activity_main`

--- app/src/main/java/com/willowtree/matthewcorbett/project_sanic/MainActivity.java
+++ app/src/main/java/com/willowtree/matthewcorbett/project_sanic/MainActivity.java
@@ -1,19 +1,29 @@
 package com.willowtree.matthewcorbett.project_sanic;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView loginText;
 
     private Button submitButton;
     private EditText usernameText;
     private EditText passwordText;
     private CheckBox usernameChecked;
+
+    private TextInputLayout usernameTextInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,26 @@ public class MainActivity extends AppCompatActivity {
         usernameText = findViewById(R.id.username_text);
         passwordText = findViewById(R.id.password_text);
         usernameChecked = findViewById(R.id.save_username);
+        loginText = findViewById(R.id.login_text);
+
+        usernameTextInput = findViewById(R.id.username_text_input);
+
+        usernameText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
         /* Set a listener on the button so that we can do something
         when the user clicks
