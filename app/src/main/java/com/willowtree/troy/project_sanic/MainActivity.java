861fadf2a322c1d55db4074be9f8977408f9c34e
Troy Marchesseault <tmarche@ncsu.edu>
2019-10-07 22:03:54 -0400

    Add submit toast; fix length issue with handling special names

--- app/src/main/java/com/willowtree/troy/project_sanic/MainActivity.java
+++ app/src/main/java/com/willowtree/troy/project_sanic/MainActivity.java
@@ -18,43 +18,32 @@ public class MainActivity extends AppCompatActivity{
     private int MAX_USERNAME_LENGTH = 16;
     private int MIN_PASSWORD_LENGTH = 8;
     private List<String> BAD_NAMES = Arrays.asList("emily", "ejasieno", "eijasien");
-    private List<String> GOOD_NAMES = Arrays.asList("troy", "tmarches", "matt");
+    private List<String> GOOD_NAMES = Arrays.asList("justin", "troy", "tmarches", "matt");
+    private String PASS_ERROR = "Password must be at least " + MIN_PASSWORD_LENGTH +
+            " characters long";
 
+    private TextInputLayout usernameEditText;
+    private TextInputLayout passwordEditText;
+    private RadioGroup radioGroup;
+    private CheckBox saveUsername;
+    private CheckBox savePassword;
+    private Switch shareLocation;
     private Button submitButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mycoolapp);
-        setupSubmitButton();
         setupUsernameField();
         setupPasswordField();
-
-    }
-
-    private void setupSubmitButton() {
-        submitButton = findViewById(R.id.submit);
-
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Do stuff
-                Context context = getApplicationContext();
-                CharSequence text = "Hello toast!";
-                int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-            }
-        });
-    }
-
-    private void setupRadioGroup() {
-        RadioGroup radioGroup = findViewById(R.id.radiogroup);
+        setupRadioGroup();
+        setupBooleanFields();
+        setupSubmitButton();
     }
 
     private void setupUsernameField() {
-        final TextInputLayout usernameEditText = findViewById(R.id.username);
+        usernameEditText = findViewById(R.id.username);
 
         usernameEditText.getEditText().addTextChangedListener(new TextWatcher() {
             private boolean autoChange = true;
@@ -65,7 +54,6 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 EditText eText = usernameEditText.getEditText();
-                String text = eText.getText().toString();
 
                 // Check length
                 if ( s.length() > MAX_USERNAME_LENGTH) {
@@ -75,14 +63,16 @@ public class MainActivity extends AppCompatActivity{
                     usernameEditText.setError(null);
                 }
                 // Check special names
-                for ( int i = 0; i < BAD_NAMES.size(); i++) {
-                    if ( autoChange && text.toLowerCase().contains( BAD_NAMES.get(i) )) {
+                handleSpecialNames(eText, BAD_NAMES, "sucks");
+                handleSpecialNames(eText, GOOD_NAMES, "is cool");
+            }
+
+            private void handleSpecialNames(EditText eText, List<String> names, String modifier) {
+                String text = eText.getText().toString();
+                for (int i = 0; i < names.size(); i++) {
+                    if (autoChange && text.toLowerCase().contains( names.get(i))) {
                         autoChange = false;
-                        eText.setText( BAD_NAMES.get(i) + " sucks");
-                    }
-                    if ( autoChange && text.toLowerCase().contains( GOOD_NAMES.get(i) )) {
-                        autoChange = false;
-                        eText.setText( GOOD_NAMES.get(i) + " is cool");
+                        eText.setText( names.get(i) + " " + modifier);
                     }
                 }
             }
@@ -94,7 +84,7 @@ public class MainActivity extends AppCompatActivity{
     }
 
     private void setupPasswordField() {
-        final TextInputLayout passwordEditText = findViewById(R.id.password);
+        passwordEditText = findViewById(R.id.password);
 
         // NOTE: This should occur upon clicking the submit button.
         passwordEditText.getEditText().addTextChangedListener(new TextWatcher() {
@@ -104,9 +94,8 @@ public class MainActivity extends AppCompatActivity{
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if ( s.length() < MIN_PASSWORD_LENGTH) {
-                    passwordEditText.setError("Password must be at least "
-                            + MIN_PASSWORD_LENGTH + " characters long.");
+                if ( ! isValidPassword(s) ){
+                    passwordEditText.setError(PASS_ERROR);
                 } else {
                     passwordEditText.setError(null);
                 }
@@ -116,6 +105,66 @@ public class MainActivity extends AppCompatActivity{
             public void afterTextChanged(Editable s) {}
 
         });
+    }
+
+    private void setupRadioGroup() {
+        radioGroup = findViewById(R.id.radiogroup);
+    }
+
+    private void setupBooleanFields() {
+        saveUsername = findViewById(R.id.save_user);
+        savePassword = findViewById(R.id.save_password);
+        shareLocation = findViewById(R.id.share_location);
+    }
+
+    private void setupSubmitButton() {
+        submitButton = findViewById(R.id.submit);
+        final TextInputLayout passwordEditText = findViewById(R.id.password);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Do stuff
+                Context context = getApplicationContext();
+                CharSequence text;
+                int duration = Toast.LENGTH_LONG;
+
+                // Check if password meets requirements
+                if ( ! isValidPassword(passwordEditText.getEditText().getText() )) {
+                    text = PASS_ERROR;
+                } else {
+                    text = generateToastString();
+                }
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+    }
+
+    public boolean isValidPassword(CharSequence s) {
+        return s.length() >= MIN_PASSWORD_LENGTH;
+    }
+
+    private String generateToastString() {
+        String username = usernameEditText.getEditText().getText().toString();
+        String password = passwordEditText.getEditText().getText().toString();
+        int checked = radioGroup.getCheckedRadioButtonId();
+        RadioButton checkedButton = findViewById(checked);
+        String fontPreference = checkedButton.getText().toString();
+        String saveUser = Boolean.toString(saveUsername.isChecked());
+        String savePass = Boolean.toString(savePassword.isChecked());
+        String shareLoc = Boolean.toString(shareLocation.isChecked());
+
+        return String.format("Username: %s\nPassword: %s\nFont: %s\nSave Username: %s\n" +
+                        "Save Password: %s\nShare Location: %s",
+                username,
+                password,
+                fontPreference,
+                saveUser,
+                savePass,
+                shareLoc);
     }
 
     @Override
